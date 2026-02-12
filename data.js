09a5bbe6e19b23a63b0c9e0f69acf6b4062f9636
kihuman22-blip <kihuman22@gmail.com>
2026-02-12 23:35:34 +0100

    neuer

--- data.js
+++ data.js
@@ -413,37 +413,6 @@ var APP_DATA = {
         }
       ],
       "infoHotspots": []
-    },
-    {
-      "id": "10-20260212_215619_015",
-      "name": "20260212_215619_015",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1488,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
     }
   ],
   "name": "Project Title",
